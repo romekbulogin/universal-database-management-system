09d2b505dffa563fd67275610546051505c00147
Roman Bulygin <roman.bulyg@gmail.com>
2023-01-12 18:38:06 +0300

    Added iml file

---
+++ Authorization-Service/src/test/kotlin/ru/edu/authorizationservice/AuthorizationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.edu.authorizationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ Database-Manager/src/test/kotlin/ru/edu/databasemanager/DatabaseManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.edu.databasemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatabaseManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
